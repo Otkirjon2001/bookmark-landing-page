a3d5ab6b2a9997a4dc7fff150e33930af2b1f4fc
Otkirjon2001 <otkirsalimboyev@gmail.com>
2022-06-24 13:29:42 +0300

    Add other styles

--- js/main.js
+++ js/main.js
@@ -1,34 +1,39 @@
-const elSiteButton1 = document.getElementById("features-item-button-1");
-const elSiteButton2 = document.getElementById("features-item-button-2");
-const elSiteButton3 = document.getElementById("features-item-button-3");
-const elSiteFeatures1 = document.querySelector(".features-wrapper-box-1");
-const elSiteFeatures2 = document.querySelector(".features-wrapper-box-2");
-const elSiteFeatures3 = document.querySelector(".features-wrapper-box-3");
+const elsFeaturesButtons = document.querySelectorAll('.js-features-button');
+const elsFeaturesItem = document.querySelectorAll('.js-features-item');
+const elsTabsItem = document.querySelectorAll('.js-tabs-box');
+const elsAccordionItemToggler = document.querySelectorAll('.accordion__item-toggler');
+const elsAccordionItem = document.querySelectorAll('.accordion__item');
 
-elSiteButton1.addEventListener("click", function() {
-  elSiteButton1.classList.add("features__button--active");
-  elSiteButton2.classList.remove("features__button--active");
-  elSiteButton3.classList.remove("features__button--active");
-  elSiteFeatures1.classList.remove("none");
-  elSiteFeatures2.classList.add("none");
-  elSiteFeatures3.classList.add("none");
+elsFeaturesButtons.forEach(function(elFeaturesButton){
+  elFeaturesButton.addEventListener('click', function(){
+    function deactivateFeaturesItem (){
+      elsFeaturesItem.forEach(function(elFeaturesItem){
+        elFeaturesItem.classList.remove('features__button--active');
+      })
+
+    }
+    deactivateFeaturesItem();
+
+    elFeaturesButton.parentElement.classList.add('features__button--active');
+  })
+
 })
-elSiteButton2.addEventListener("click", function() {
-  elSiteButton1.classList.remove("features__button--active");
-  elSiteButton2.classList.add("features__button--active");
-  elSiteButton3.classList.remove("features__button--active");
-  elSiteFeatures1.classList.add("none");
-  elSiteFeatures2.classList.remove("none");
-  elSiteFeatures3.classList.add("none");
-})
-elSiteButton3.addEventListener("click", function() {
-  elSiteButton1.classList.remove("features__button--active");
-  elSiteButton2.classList.remove("features__button--active");
-  elSiteButton3.classList.add("features__button--active");
-  elSiteFeatures1.classList.add("none");
-  elSiteFeatures2.classList.add("none");
-  elSiteFeatures3.classList.remove("none");
+elsFeaturesButtons.forEach(function(elFeaturesButton){
+  elFeaturesButton.addEventListener('click', function(){
+    elsTabsItem.forEach(function(elTabsItem){
+      elTabsItem.classList.remove('tabs__box--active');
+    })
+    const elTargetPanel = document.querySelector(`#${elFeaturesButton.dataset.title}`);
+    elTargetPanel.classList.add('tabs__box--active');
+  })
 })
 
-const elsParagraph = document.querySelectorAll('p');
-console.log(elsParagraph.length);
+
+elsAccordionItemToggler.forEach(function(elAccordionItemToggler){
+  elAccordionItemToggler.addEventListener('click', function(){
+    elsAccordionItem.forEach(function(elAccordionItem){
+      elAccordionItem.classList.remove('accordion__item--open');
+    })
+    elAccordionItemToggler.closest('.accordion__item').classList.add('accordion__item--open');
+  })
+})
